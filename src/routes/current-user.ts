4e833a9150e3fe4327bfaa665dc8384b7d4db82f
Ramprashant Sarma <prashanth.sarma@gmail.com>
2020-09-07 10:44:44 +0530

    Enabling auto login first time while opening

--- src/routes/current-user.ts
+++ src/routes/current-user.ts
@@ -4,7 +4,7 @@ import { currentUser } from '@prashanthsarma/property-portal-common';
 const router = express.Router();
 
 router.get('/api/users/currentuser', currentUser, (req, res) => {
-  res.send({ currentUser: req.currentUser || null });
+  res.send(req.currentUser || null );
 });
 
 export { router as currentUserRouter };
