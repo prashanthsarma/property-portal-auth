8abe7855e576ff73d9a9c48f3eecbfc171f85993
Ramprashant Sarma <prashanth.sarma@gmail.com>
2020-09-07 12:24:42 +0530

    pointing to always 5000 is production deployment

--- src/index.ts
+++ src/index.ts
@@ -19,8 +19,8 @@ const start = async () => {
     console.error(err);
   }
 
-  app.listen(4000, () => {
-    console.log('Auth listening on port 4000!');
+  app.listen(5000, () => {
+    console.log('Auth listening on port 5000!');
   });
 };
 
