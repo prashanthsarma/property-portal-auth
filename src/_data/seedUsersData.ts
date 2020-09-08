c3ef4224055e0718d01839e92ce5457085b8ff7f
Ramprashant Sarma <prashanth.sarma@gmail.com>
2020-09-09 00:12:53 +0530

    Various bug fixing

--- src/_data/seedUsersData.ts
+++ src/_data/seedUsersData.ts
@@ -1,11 +1,11 @@
 import { UserDoc, User } from "../models/user";
-import { LoginMode } from "@prashanthsarma/property-portal-common";
+import { LoginMode, IUserAttrs } from "@prashanthsarma/property-portal-common";
 
 export const seedUserData: UserDoc[] = [
   User.build({
-    // "_id": "5d7a514b5d2c12c7449be043",
+    "_id": "5d7a514b5d2c12c7449be043",
     "email": "seeder@default.com",
     "password": "seeder",
     "loginMode": LoginMode.manual
-  })
+  } as IUserAttrs)
 ]
